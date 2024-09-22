29c545a4944dcd962dcd0b701072e319ebe84db3
tera330 <tera877tech@gmail.com>
2024-09-23 03:15:20 +0900

    ローディング時のUIコンポーネントの作成

---
+++ app/src/main/java/com/example/fluxsample/ui/common/IndeterminateCircularIndicator.kt
@@ -0,0 +1,41 @@
+package com.example.fluxsample.ui.common
+
+import androidx.compose.foundation.layout.Arrangement
+import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.width
+import androidx.compose.material3.CircularProgressIndicator
+import androidx.compose.material3.MaterialTheme
+import androidx.compose.runtime.Composable
+import androidx.compose.runtime.getValue
+import androidx.compose.runtime.mutableStateOf
+import androidx.compose.runtime.remember
+import androidx.compose.ui.Alignment
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.tooling.preview.Preview
+import androidx.compose.ui.unit.dp
+
+@Composable
+fun IndeterminateCircularIndicator() {
+    val loading by remember { mutableStateOf(true) }
+
+    if (!loading) return
+
+    CircularProgressIndicator(
+        modifier = Modifier.width(64.dp),
+        color = MaterialTheme.colorScheme.secondary,
+        trackColor = MaterialTheme.colorScheme.surfaceVariant,
+    )
+}
+
+@Preview
+@Composable
+fun IndeterminateCircularIndicatorPreview() {
+    Column(
+        modifier = Modifier.fillMaxSize(),
+        horizontalAlignment = Alignment.CenterHorizontally,
+        verticalArrangement = Arrangement.Center,
+    ) {
+        IndeterminateCircularIndicator()
+    }
+}

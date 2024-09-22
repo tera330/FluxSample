c1c8ab3da28759a820c2b742207dc2a06027a0de
tera330 <tera877tech@gmail.com>
2024-09-23 03:16:54 +0900

    フォーマット修正

--- app/src/main/java/com/example/fluxsample/flux/fetchweather/FetchWeatherActionCreator.kt
+++ app/src/main/java/com/example/fluxsample/flux/fetchweather/FetchWeatherActionCreator.kt
@@ -6,27 +6,27 @@ import com.example.fluxsample.flux.fetchweather.data.FetchWeatherRepository
 import javax.inject.Inject
 
 class FetchWeatherActionCreator
-@Inject
-constructor(
-    private val dispatcher: Dispatcher,
-    private val repository: FetchWeatherRepository,
-) {
-    suspend fun fetchWeatherByCityName(cityName: String) {
-        dispatcher.dispatch(FetchWeatherAction.Loading)
-        try {
-            val response =
-                repository
-                    .fetchWeatherByCityName(cityName)
-                    ?.weather
-                    ?.get(0)
-                    ?.description
-            val weather = response.toString()
-            dispatcher.dispatch(FetchWeatherAction.Success(weather))
-        } catch (e: Exception) {
-            dispatcher.dispatch(FetchWeatherAction.Failure("Failed to fetch weather."))
+    @Inject
+    constructor(
+        private val dispatcher: Dispatcher,
+        private val repository: FetchWeatherRepository,
+    ) {
+        suspend fun fetchWeatherByCityName(cityName: String) {
+            dispatcher.dispatch(FetchWeatherAction.Loading)
+            try {
+                val response =
+                    repository
+                        .fetchWeatherByCityName(cityName)
+                        ?.weather
+                        ?.get(0)
+                        ?.description
+                val weather = response.toString()
+                dispatcher.dispatch(FetchWeatherAction.Success(weather))
+            } catch (e: Exception) {
+                dispatcher.dispatch(FetchWeatherAction.Failure("Failed to fetch weather."))
+            }
         }
     }
-}
 
 sealed class FetchWeatherAction : Action {
     data object Initial : FetchWeatherAction() {

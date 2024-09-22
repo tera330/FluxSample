b7cdad0eb37c0a3b51deaebdbf19ff5d5661627c
tera330 <tera877tech@gmail.com>
2024-09-23 02:07:18 +0900

    天気取得画面のUI作成

--- app/src/main/java/com/example/fluxsample/MainActivity.kt
+++ app/src/main/java/com/example/fluxsample/MainActivity.kt
@@ -4,13 +4,19 @@ import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
 import androidx.activity.enableEdgeToEdge
+import androidx.compose.foundation.layout.Arrangement
+import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.Spacer
 import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.layout.padding
+import androidx.compose.material3.Button
 import androidx.compose.material3.Scaffold
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
+import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
-import androidx.compose.ui.tooling.preview.Preview
+import androidx.compose.ui.unit.dp
+import androidx.compose.ui.unit.sp
 import com.example.fluxsample.ui.theme.FluxSampleTheme
 
 class MainActivity : ComponentActivity() {
@@ -20,8 +26,7 @@ class MainActivity : ComponentActivity() {
         setContent {
             FluxSampleTheme {
                 Scaffold(modifier = Modifier.fillMaxSize()) { innerPadding ->
-                    Greeting(
-                        name = "Android",
+                    FluxSampleApp(
                         modifier = Modifier.padding(innerPadding),
                     )
                 }
@@ -31,20 +36,24 @@ class MainActivity : ComponentActivity() {
 }
 
 @Composable
-fun Greeting(
-    name: String,
-    modifier: Modifier = Modifier,
-) {
-    Text(
-        text = "Hello $name!",
-        modifier = modifier,
-    )
-}
+fun FluxSampleApp(modifier: Modifier) {
+    Column(
+        modifier = modifier.fillMaxSize(),
+        horizontalAlignment = Alignment.CenterHorizontally,
+        verticalArrangement = Arrangement.Center,
+    ) {
+        Button(onClick = { }) {
+            Text(
+                text = "天気を取得",
+                fontSize = 30.sp,
+            )
+        }
 
-@Preview(showBackground = true)
-@Composable
-fun GreetingPreview() {
-    FluxSampleTheme {
-        Greeting("Android")
+        Spacer(modifier = Modifier.padding(50.dp))
+
+        Text(
+            text = "まだ天気を取得していません",
+            fontSize = 25.sp,
+        )
     }
 }
